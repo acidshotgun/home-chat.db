b9342def7370ccd6a1ce190b82a13d914abcb4d8
Sasha <example@gmail.com>
2023-04-09 21:09:18 +0300

    bad post request reloading brow

--- js/script.js
+++ js/script.js
@@ -5,20 +5,53 @@ const btnSubmit = document.querySelector('.submit'),
       theme = document.querySelector('.theme');
 
 
-btnSubmit.addEventListener('click', () => {
+btnSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
     postData();
     input.value = '';
 });
 
 function postData() {
-    axios.post('http://localhost:3000/comments', {
+    axios.post('http://localhost:3000/posts', {
         theme: theme.value,
-        body: input.value,
+        description: input.value,
     })
     .then(response => console.log(response.data))
+    .then(new Post(post, 'test', 'descr').render())
     .catch(() => {
         alert('ERROR');
     })
     .finaly(console.log('End'));
 }
+
+
+const post = document.querySelector('.post__container');
+
+class Post {
+    constructor(parentSelector, name, descr) {
+        this.parentSelector = parentSelector;
+        // this.img = img;
+        this.name = name;
+        this.descr = descr;
+    }
+
+    render() {
+        const element = document.createElement('div');
+        element.classList.add('post__item');
+        
+        element.innerHTML = `
+                <div class="post__img">
+                    <img class="post__img-pic" src="../img/cat.jpg" alt="alt">
+                </div>
+                <div class="post__content">
+                    <div class="post__name">${this.name}</div>
+                    <div class="post__descr">${this.descr}</div>
+                </div>
+            `;
+
+        this.parentSelector.append(element);
+    }
+}
+
+const cat = new Post(post, 'test', 'descr').render();
 
